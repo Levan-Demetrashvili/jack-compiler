35d78f47c24cd1f2a8deedbbe51a867f997bcf93
Levan-Demetrashvili <levandemetrashvili10@gmail.com>
2025-07-19 16:20:22 +0400

    refactoring compilation engine code

--- compilation_engine.py
+++ compilation_engine.py
@@ -1,233 +1,237 @@
 from variables import jack_compile_rules
 
-index = 0
+index = None
+f = None
+tokens = []
 
-def compilation_engine(filepath,tokens):
-  global index 
+def compilation_engine(filepath,tokens_stream):
+  global index,tokens,f
   index = 0
-  with open(filepath,'w') as f:
-    compile_class(f,tokens)
+  with open(filepath,'w') as file:
+    f = file
+    tokens = tokens_stream
+    compile_class()
   return
     
-def compile_class(f,tokens):
+def compile_class():
   f.write('<class>' + '\n')
-  validate({'class'},tokens[index],f)
-  validate({'identifier'}, tokens[index],f)
-  validate({'{'}, tokens[index],f)
+  validate({'class'})
+  validate({'identifier'})
+  validate({'{'})
   while tokens[index][0] in jack_compile_rules["class_var_dec_keywords"]:
-    compile_class_var_dec(f,tokens)
+    compile_class_var_dec()
   while tokens[index][0] in jack_compile_rules["subroutines_dec_keywords"]:
-    compile_subroutine_dec(f,tokens)
-  validate({'}'}, tokens[index],f)
+    compile_subroutine_dec()
+  validate({'}'})
   f.write('</class>')
   return
 
 
-def compile_class_var_dec(f,tokens):
+def compile_class_var_dec():
   f.write(f'<classVarDec>\n')
-  validate(jack_compile_rules["class_var_dec_keywords"], tokens[index],f)
-  validate(jack_compile_rules["type"], tokens[index],f)
-  validate({'identifier'}, tokens[index],f)
+  validate(jack_compile_rules["class_var_dec_keywords"])
+  validate(jack_compile_rules["type"])
+  validate({'identifier'})
   while tokens[index][0]  == ',':
-    validate({','}, tokens[index],f)
-    validate({'identifier'}, tokens[index],f)
-  validate({';'}, tokens[index],f)
+    validate({','})
+    validate({'identifier'})
+  validate({';'})
   f.write(f'</classVarDec>\n')
   return
 
-def compile_subroutine_dec(f,tokens):
+def compile_subroutine_dec():
   f.write(f'<subroutineDec>\n')
-  validate(jack_compile_rules["subroutines_dec_keywords"], tokens[index],f)
-  validate({'void',*jack_compile_rules["type"]}, tokens[index],f)
-  validate({'identifier'}, tokens[index],f)
-  validate({'('}, tokens[index],f)
-  compile_parameter_list(f,tokens)
-  validate({')'}, tokens[index],f)
+  validate(jack_compile_rules["subroutines_dec_keywords"])
+  validate({'void',*jack_compile_rules["type"]})
+  validate({'identifier'})
+  validate({'('})
+  compile_parameter_list()
+  validate({')'})
   
-  compile_subroutine_body(f,tokens)
+  compile_subroutine_body()
   
   f.write(f'</subroutineDec>\n')
 
-def compile_parameter_list(f,tokens):
+def compile_parameter_list():
   f.write(f'<parameterList>\n')
   if tokens[index][0] != ')':
-    validate(jack_compile_rules["type"], tokens[index],f)
-    validate({'identifier'}, tokens[index],f)
+    validate(jack_compile_rules["type"])
+    validate({'identifier'})
     while tokens[index][0]  == ',':
-      validate({','}, tokens[index],f)
-      validate(jack_compile_rules["type"], tokens[index],f)
-      validate({'identifier'}, tokens[index],f)
+      validate({','})
+      validate(jack_compile_rules["type"])
+      validate({'identifier'})
     
   f.write(f'</parameterList>\n')
 
-def compile_subroutine_body(f,tokens):
+def compile_subroutine_body():
   f.write(f'<subroutineBody>\n')
   
-  validate({'{'}, tokens[index],f)
+  validate({'{'})
   while tokens[index][0]  == 'var':
-    compile_var_dec(f,tokens)
+    compile_var_dec()
   
-  compile_statements(f,tokens)
-  validate({'}'}, tokens[index],f)
+  compile_statements()
+  validate({'}'})
   
   f.write(f'</subroutineBody>\n')
  
 
-def compile_var_dec(f,tokens):
+def compile_var_dec():
   f.write(f'<varDec>\n')
-  validate({'var'}, tokens[index],f)
-  validate(jack_compile_rules["type"], tokens[index],f)
-  validate({'identifier'}, tokens[index],f)
+  validate({'var'})
+  validate(jack_compile_rules["type"])
+  validate({'identifier'})
   while tokens[index][0]  == ',':  
-    validate({','}, tokens[index],f)
-    validate({'identifier'}, tokens[index],f)
-  validate({';'}, tokens[index],f)
+    validate({','})
+    validate({'identifier'})
+  validate({';'})
   f.write(f'</varDec>\n')
   
  
-def compile_statements(f,tokens):
+def compile_statements():
   f.write(f'<statements>\n')
   while tokens[index][0] in jack_compile_rules["statements_keywords"]:
     func_name = f"compile_{tokens[index][0]}"
     func = globals()[func_name] 
-    func(f, tokens)
+    func()
   f.write(f'</statements>\n')
   
   
-def compile_let(f,tokens):
+def compile_let():
   f.write(f'<letStatement>\n')
 
-  validate({'let'}, tokens[index],f)
-  validate({'identifier'}, tokens[index],f)
+  validate({'let'})
+  validate({'identifier'})
   
   if tokens[index][0] == '[':
-    validate({'['}, tokens[index],f)
-    compile_expression(f,tokens)
-    validate({']'}, tokens[index],f)
+    validate({'['})
+    compile_expression()
+    validate({']'})
   
-  validate({'='}, tokens[index],f)
-  compile_expression(f,tokens)
-  validate({';'}, tokens[index],f)
+  validate({'='})
+  compile_expression()
+  validate({';'})
   
   f.write(f'</letStatement>\n')
   
-def compile_if(f,tokens):
+def compile_if():
   f.write(f'<ifStatement>\n')
 
-  validate({'if'}, tokens[index],f)
-  validate({'('}, tokens[index],f)
-  compile_expression(f,tokens)
-  validate({')'}, tokens[index],f)
+  validate({'if'})
+  validate({'('})
+  compile_expression()
+  validate({')'})
   
-  validate({'{'}, tokens[index],f)
-  compile_statements(f,tokens)
-  validate({'}'}, tokens[index],f)
+  validate({'{'})
+  compile_statements()
+  validate({'}'})
   
   if tokens[index][0] == 'else':
-    validate({'else'}, tokens[index],f)
-    validate({'{'}, tokens[index],f)
-    compile_statements(f,tokens)
-    validate({'}'}, tokens[index],f)
+    validate({'else'})
+    validate({'{'})
+    compile_statements()
+    validate({'}'})
   
   f.write(f'</ifStatement>\n')
 
-def compile_while(f,tokens):
+def compile_while():
   f.write(f'<whileStatement>\n')
   
-  validate({'while'}, tokens[index],f)
-  validate({'('}, tokens[index],f)
-  compile_expression(f,tokens)
-  validate({')'}, tokens[index],f)
+  validate({'while'})
+  validate({'('})
+  compile_expression()
+  validate({')'})
   
-  validate({'{'}, tokens[index],f)
-  compile_statements(f,tokens)
-  validate({'}'}, tokens[index],f)
+  validate({'{'})
+  compile_statements()
+  validate({'}'})
     
   f.write(f'</whileStatement>\n')
   
 
-def compile_do(f,tokens):
+def compile_do():
   f.write(f'<doStatement>\n')
   
-  validate({'do'}, tokens[index],f)
-  validate({'identifier'}, tokens[index],f)
+  validate({'do'})
+  validate({'identifier'})
   if tokens[index][0] == '.':
-    validate({'.'}, tokens[index],f)
-    validate({'identifier'}, tokens[index],f)
+    validate({'.'})
+    validate({'identifier'})
   
-  validate({'('}, tokens[index],f)
-  compile_expression_list(f,tokens)
-  validate({')'}, tokens[index],f)
-  validate({';'}, tokens[index],f)
+  validate({'('})
+  compile_expression_list()
+  validate({')'})
+  validate({';'})
     
   f.write(f'</doStatement>\n')
 
-def compile_return(f,tokens):
+def compile_return():
   f.write(f'<returnStatement>\n')
   
-  validate({'return'}, tokens[index],f)
+  validate({'return'})
   
   if tokens[index][0] != ';':
-    compile_expression(f,tokens)
+    compile_expression()
     
-  validate({';'}, tokens[index],f)
+  validate({';'})
   
   f.write(f'</returnStatement>\n')
   
 
-def compile_expression(f,tokens):
+def compile_expression():
   f.write(f'<expression>\n')
-  compile_term(f,tokens)
+  compile_term()
   
   if tokens[index][0] in jack_compile_rules["op_symbols"]:
-    validate(jack_compile_rules["op_symbols"], tokens[index],f)
-    compile_term(f,tokens)
+    validate(jack_compile_rules["op_symbols"])
+    compile_term()
   
   f.write(f'</expression>\n')
 
-def compile_term(f,tokens):
+def compile_term():
   f.write(f'<term>\n')
   previous_token = tokens[index][2]
   constants = {'stringConstant', 'integerConstant', '(' ,*jack_compile_rules["keyword_constants"],*jack_compile_rules["unary_op_symbols"]}  
-  validate({'identifier', *constants}, tokens[index],f)
+  validate({'identifier', *constants})
   if previous_token == '(':
-    compile_expression(f,tokens)
-    validate({')'}, tokens[index],f)
+    compile_expression()
+    validate({')'})
   elif previous_token == 'identifier':
     match tokens[index][2]:
       case '[':
-        validate({'['}, tokens[index],f)
-        compile_expression(f,tokens)
-        validate({']'}, tokens[index],f)
+        validate({'['})
+        compile_expression()
+        validate({']'})
       case '(':
-        validate({'('}, tokens[index],f)
-        compile_expression_list(f,tokens)
-        validate({')'}, tokens[index],f)
+        validate({'('})
+        compile_expression_list()
+        validate({')'})
       case '.':
-        validate({'.'}, tokens[index],f)
-        validate({'identifier'}, tokens[index],f)
-        validate({'('}, tokens[index],f)
-        compile_expression_list(f,tokens)
-        validate({')'}, tokens[index],f)
+        validate({'.'})
+        validate({'identifier'})
+        validate({'('})
+        compile_expression_list()
+        validate({')'})
   elif previous_token in jack_compile_rules["unary_op_symbols"]:  
-    compile_term(f,tokens)
+    compile_term()
   f.write(f'</term>\n')
 
-def compile_expression_list(f,tokens):
+def compile_expression_list():
   f.write(f'<expressionList>\n')
   
   if tokens[index][0] != ')':
-    compile_expression(f,tokens)
+    compile_expression()
     while tokens[index][0] == ',':
-      validate({','}, tokens[index],f)
-      compile_expression(f,tokens)
+      validate({','})
+      compile_expression()
     
   f.write(f'</expressionList>\n')
   
 
-def validate(valid_values,current_token,f):
+def validate(valid_values):
   global index 
-  t,type_,check_value = current_token
+  t,type_,check_value = tokens[index]
   if check_value in valid_values:
     f.write(f"<{type_}> {t} </{type_}>\n")
     index += 1
